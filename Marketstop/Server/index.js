97a691784d51347888a292765050fd143195478e
Beauxman <76933735+Beauxman@users.noreply.github.com>
2022-05-14 23:46:34 -0700

    Add files via upload

--- Marketstop/Server/index.js
+++ Marketstop/Server/index.js
@@ -10,58 +10,6 @@ app.set('port', 3000)
 app.use(express.json())
 app.use(cors())
 
-app.get('/api/students/:id', function(req, res){
-	console.log(`${req.params.id}`)
-	MongoClient.connect(url, function(err, conn) {
-		if (err) console.log(err)
-		else {
-			const db = conn.db('campus-02')
-			const coll = db.collection('students')
-			const criteria = {_id: new mongo.ObjectID(req.params.id)}
-			coll.find(criteria).toArray(function(err, result) {
-				if (err) console.log(err)
-				else {
-					conn.close()
-					// Send the data back 
-					res.type('application/json')
-					res.status(200)
-					res.json(result)					
-				}
-			})
-		}
-	})
-})
-
-app.post('/api/students', function(req, res) {
-	console.log(req.body)
-	MongoClient.connect(url, function(err, conn) {
-		if (err) console.log(err)
-		else {
-			const db = conn.db('campus-02')
-			// Prepare the JS myObj 
-			const myObj = new Object()
-			myObj.firstName = req.body.firstName
-			myObj.lastName = req.body.lastName
-			const dt_flds = req.body.dob.split('-')
-			const yyyy = dt_flds[0]
-			const mm = dt_flds[1]
-			const dd = dt_flds[2] 
-			myObj.dob = new Date(yyyy, mm, dd)
-			const coll = db.collection('students')
-			coll.insertOne(myObj, function(err, result) {
-				if (err) console.log(err)
-				else {
-					conn.close()
-					// Send the data back 
-					res.type('application/json')
-					res.status(200)
-					res.json(result)					
-				}
-			})
-		}
-	})
-})
-
 app.get('/api/accounts', function(req, res){
 	//console.log(`${req.query.email}`)
 	//console.log(`${req.query.password}`)
